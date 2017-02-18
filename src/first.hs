8a1cfe051080be7d44b1543c25e2c1676e1e8bb0
RinesThaix <iam@kostya.sexy>
2017-02-18 12:59:12 +0300

    Первый прак #2

--- src/first.hs
+++ src/first.hs
@@ -14,7 +14,28 @@ isEmpty s
     | isSpace (head s)  = isEmpty (drop 1 s)
     | otherwise         = 1
 
+-- Тупая сортировка
+
+insert  :: (Ord a) => a -> [a] -> [a]
+insert' :: (Ord a) => a -> [a] -> [a] -> [a]
+sort    :: (Ord a) => [a] -> [a]
+sort'   :: (Ord a) => [a] -> [a] -> [a]
+
+insert el l             = insert' el [] l
+
+insert' el l1 l2
+    | length l2 == 0    = l1 ++ [el]
+    | el < head l2      = l1 ++ [el] ++ l2
+    | otherwise         = insert' el (l1 ++ [(head l2)]) (drop 1 l2)
+
+sort l                  = sort' [] l
+
+sort' l1 l2
+    | length l2 == 0    = l1
+    | otherwise         = sort' (insert (head l2) l1) (drop 1 l2)
+
 main = do
     let test = ["Hello", "", "Who art thou", "", "  ", "Hi", "\n\n\n", "2\n "]
     print $ tinky 0 test
     print $ sum (map (\s -> isEmpty s) test)
+    print $ sort [1, 2, 16, 13, 8, -5, 16, 22, 19, 64, 0]
